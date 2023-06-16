a6d142a18179c6cfd3d3d83340acc7ff334e46d1
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-06-16 13:43:50 +1000

    add extra testing of get_present

--- tests/testthat/test-get_present.R
+++ tests/testthat/test-get_present.R
@@ -17,3 +17,53 @@ test_that("basic example", {
 
   expect_equal(present_val, present_goal)
 })
+
+test_that("simple example", {
+
+  dnabin_size_2 <- ape::as.DNAbin(
+    list(
+      "leaf_2023-08-04" = c("a", "a"),
+      "leaf_2023-08-04" = c("a", "a")
+    )
+  )
+  seqs_size_2 <- rename_dates_to_times_a(dnabin_size_2)
+  present_val_size_2 <- get_present(dnabin_size_2, seqs_size_2)
+  present_goal_2 <- list(
+    date = as.Date("2023-08-04"),
+    hours = (2/3) * 24
+  )
+  expect_equal(present_val_size_2, present_goal_2, tolerance = 1e-6)
+
+
+  dnabin_size_3 <- ape::as.DNAbin(
+    list(
+      "leaf_2023-08-04" = c("a", "a"),
+      "leaf_2023-08-04" = c("a", "a"),
+      "leaf_2023-08-04" = c("a", "a")
+    )
+  )
+  seqs_size_3 <- rename_dates_to_times_a(dnabin_size_3)
+  present_val_size_3 <- get_present(dnabin_size_3, seqs_size_3)
+  present_goal_3 <- list(
+    date = as.Date("2023-08-04"),
+    hours = (3/5) * 24
+  )
+  expect_equal(present_val_size_3, present_goal_3, tolerance = 1e-6)
+
+
+  dnabin_size_4 <- ape::as.DNAbin(
+    list(
+      "leaf_2023-08-04" = c("a", "a"),
+      "leaf_2023-08-04" = c("a", "a"),
+      "leaf_2023-08-04" = c("a", "a"),
+      "leaf_2023-08-04" = c("a", "a")
+    )
+  )
+  seqs_size_4 <- rename_dates_to_times_a(dnabin_size_4)
+  present_val_size_4 <- get_present(dnabin_size_4, seqs_size_4)
+  present_goal_4 <- list(
+    date = as.Date("2023-08-04"),
+    hours = (4/5) * 24
+  )
+  expect_equal(present_val_size_4, present_goal_4, tolerance = 1e-6)
+})
