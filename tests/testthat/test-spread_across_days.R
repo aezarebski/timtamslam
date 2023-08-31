d23f9caeac4ba08be48a4f00d4f966acb9fcf030
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-08-31 17:02:16 +1000

    implement spread time series function

--- tests/testthat/test-spread_across_days.R
+++ tests/testthat/test-spread_across_days.R
@@ -31,3 +31,35 @@ test_that("basic example", {
 
   expect_equal(daily_data_computed, daily_data_expected)
 })
+
+test_that("another example", {
+  week_start_dates <-
+    lubridate::ymd("2023-08-13") + lubridate::days((0:2) * 7)
+  week_end_dates <-
+    week_start_dates + lubridate::days(6)
+
+  weekly_data <- data.frame(
+    week_start = week_start_dates,
+    week_end = week_end_dates,
+    count = c(0,NA,53)
+  )
+  weekly_data <- weekly_data[c(1, 3), ]
+
+  daily_data_expected <-
+    data.frame(
+      date = c(
+        seq(from = lubridate::ymd("2023-08-13"),
+            to = lubridate::ymd("2023-08-19"),
+            by = 1),
+        seq(from = lubridate::ymd("2023-08-27"),
+            to = lubridate::ymd("2023-09-02"),
+            by = 1)
+      ),
+      count = c(c(0,0,0,0,0,0,0),
+                c(7,7,8,8,8,8,7))
+    )
+
+  daily_data_computed <- spread_across_days(weekly_data)
+
+  expect_equal(daily_data_computed, daily_data_expected)
+})
